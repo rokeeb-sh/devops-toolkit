756eda26d6e5b6ae78ba37013993ebc902485bdf
Rokeeb Shobiye <workwithrokeeb@gmail.com>
2025-07-24 16:08:24 +0100

    Create config.php

---
+++ docker-services/phpmyadmin/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$i = 0;
+
+$i++;
+$cfg["Servers"][$i]["host"] = "mysql";
+$cfg["Servers"][$i]["port"] = "3306";
+$cfg["Servers"][$i]["user"] = "root";
+$cfg["Servers"][$i]["password"] = "root";
+$cfg["Servers"][$i]["auth_type"] = "config";
+
+// other configs below
